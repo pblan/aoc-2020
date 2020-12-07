c1767db0f4f4d0a9b675888dc35b636d099bb8b1
Patrick Gustav Blaneck <Blaneck@itc.rwth-aachen.de>
2020-12-07 09:47:44 +0100

    06 done

---
+++ 06/customcustoms.py
@@ -0,0 +1,31 @@
+with open ('input.txt', 'r') as f:
+    groups = [answers.strip() for answers in f.read().split('\n\n')]
+
+print("--- Day 6: Custom Customs ---")
+yes = 0
+for group in groups:
+    group_yes = set()
+    for member in group.split('\n'):
+        for char in member:
+            group_yes.add(char)
+    yes += len(group_yes)
+
+print("--- Part One ---")
+print("Total count: ", yes)
+
+overall_group_yes = 0
+for group in groups:
+    group_yes = set()
+    set_initialised = False
+    for member in group.split('\n'):
+        member_yes_set = set([char for char in member])
+        if not set_initialised:
+            group_yes = member_yes_set
+            set_initialised = True
+        else:
+            group_yes = member_yes_set.intersection(group_yes)
+
+    overall_group_yes += len(group_yes)
+
+print("--- Part One ---")
+print("Total count: ", overall_group_yes)
